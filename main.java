54e3c60413e1c783cf7028f9b6c36125bea91dee
BaileyMann <bailey.mann@vtsd.com>
2023-01-20 11:45:45 -0500

    Start 2

---
+++ main.java
@@ -0,0 +1,32 @@
+public class main {
+    public static void main(String[]args){
+
+
+        System.out.print("Find out what ice cream you are");
+        System.out.print("What is your favorite color?");
+        String colorlist[] = {"blue", "green", "red"};
+        System.out.print(colorlist);
+        int color1 = lor.blue;
+        if(colorlist = color1){
+
+        } else if(colorlist = green){
+
+
+        } else if(colorlist = red){
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
